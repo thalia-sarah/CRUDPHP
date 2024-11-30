7ae02da7f6bee606e2d6c4dcc07c9ed0d6765af0
thalia-sarah <ritasaparina1983@gmail.com>
2024-11-30 17:26:56 +0700

    2 tabel database

--- human_resources/human_resources (1).sql
+++ human_resources/human_resources (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 21 Nov 2024 pada 04.57
+-- Waktu pembuatan: 30 Nov 2024 pada 09.53
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -24,6 +24,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `departemen`
+--
+
+CREATE TABLE `departemen` (
+  `departemen_id` int(11) NOT NULL,
+  `nama_departemen` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `departemen`
+--
+
+INSERT INTO `departemen` (`departemen_id`, `nama_departemen`) VALUES
+(2, 'Designer'),
+(4, 'Engineer');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `karyawan`
 --
 
@@ -35,8 +54,23 @@ CREATE TABLE `karyawan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data untuk tabel `karyawan`
+--
+
+INSERT INTO `karyawan` (`karyawan_id`, `nama_karyawan`, `posisi`, `gaji`) VALUES
+(4, 'viva', 'Fronted Developer', 5000000),
+(5, 'andi', 'Fronted Developer', 7000000),
+(7, 'anton', 'UI/UX Desinger', 30000000);
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `departemen`
+--
+ALTER TABLE `departemen`
+  ADD PRIMARY KEY (`departemen_id`);
 
 --
 -- Indeks untuk tabel `karyawan`
@@ -49,10 +83,16 @@ ALTER TABLE `karyawan`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `departemen`
+--
+ALTER TABLE `departemen`
+  MODIFY `departemen_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT untuk tabel `karyawan`
 --
 ALTER TABLE `karyawan`
-  MODIFY `karyawan_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `karyawan_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
